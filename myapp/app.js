ea09197ce9f7581a5487d8f8c6257c0dbe4095dc
Terence <wrze@sina.com>
2016-11-19 14:47:49 +0800

    modify

--- myapp/app.js
+++ myapp/app.js
@@ -1,29 +1,44 @@
+/**
+ * 通过require()加载express、http、path模块，以及routes文件夹下的index.js
+ * 和user.js
+**/
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
+var http = require('http');
 var routes = require('./routes/index');
-var users = require('./routes/users');
+// var users = require('./routes/users');
 
 var app = express();
 
 // view engine setup
+// 设置views文件夹为存放视图文件的目录, 存放模板文件 __dirname为全局变量
+// 储存当前正在执行脚本所在的目录名
 app.set('views', path.join(__dirname, 'views'));
+// app.set('port', process.env.PORT || 3000);
+// 设置视图模板引擎为ejs
 app.set('view engine', 'ejs');
+// app.use(express.favicon());
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+// connect 内建的中间件，使用默认的favicon图标
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+// connect 内建中间件，在开发环境下使用，在终端显示简单日志
 app.use(logger('dev'));
+// connect内建中间件，用来解析请求体
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+// connect内建的中间件，设置根目录下public文件夹为存放img, css, js等静态文件的目录
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
-app.use('/users', users);
+
+// 下面这句可以删除
+// app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -57,15 +72,17 @@ app.use(function(err, req, res, next) {
 });
 
 
+// module.exports = function (app) {
+//   app.get('/', function (req, res) {
+//     res.redirect('/posts');
+//   });
+//   app.use('/signup', require('routes/signup'));
+//   app.use('/signin', require('./signin'));
+//   app.use('/signout', require('./signout'));
+//   app.use('/posts', require('./posts'));
+// };
 
-module.exports = function (app) {
-  app.get('/', function (req, res) {
-    res.redirect('/posts');
-  });
-  app.use('/signup', require('routes/signup'));
-  app.use('/signin', require('./signin'));
-  app.use('/signout', require('./signout'));
-  app.use('/posts', require('./posts'));
-};
 
+// module.exports = app;
+// routes(app);
 module.exports = app;
--- myapp/routes/index.js
+++ myapp/routes/index.js
@@ -1,9 +1,67 @@
 var express = require('express');
 var router = express.Router();
-
+var app = express();
 /* GET home page. */
+// 路由控制器，如果用户访问 ‘/’ 主页则由路由 router.index来处理
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    // 访问主页时，调用ejs模板引擎，渲染index.ejs模板文件
+    res.render('index', {
+        title: '主页'
+    });
 });
 
+router.get('/reg', function(req, res, next) {
+    // 访问主页时，调用ejs模板引擎，渲染index.ejs模板文件
+    res.render('index', {
+        title: '注册'
+    });
+});
+
+router.post('/reg', function(req, res, next) {
+
+});
+
+router.get('/login', function(req, res, next) {
+    res.render('login', {
+        title: '登录'
+    });
+});
+
+router.post('/login', function (req, res, next) {
+
+});
+
+router.get('/post', function (req, res) {
+    res.render('post', {title:'发表'});
+});
+
+app.post('/post', function (req, res) {
+
+});
+
+app.get('/logout', function (req, res) {
+
+});
+
+// 如何针对已登录和未登录的用户显示不同的内容呢，或者如何判断用户是否已经登录呢，
+// 通过引入会话机制，来记录用户登录状态，还需要访问数据库来保存和读取用户信息
+
+//
+//
+// // 准备把实现路由功能的代码都放在 routes/index.js里
+// // 官方给出的写法是在 app.js中实现简单的路由分配，然后去index.js找
+// // 对应的路由函数，最终实现路由功能，我们不妨把路由控制和实现路由功能的
+// // 函数都放到index.js里，app.js 中只有一个总的路由接口
+// // module.exports = function(app){
+// //     app.get('/', function (req, res) {
+// //         res.render('index', {title: 'Express'});
+// //     });
+// // };
+//
+// module.exports = router;
+// module.exports = function (app) {
+//     app.get('/', function (req, res) {
+//         res.render('index', {title: 'Express'});
+//     });
+// };
 module.exports = router;
